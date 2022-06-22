6b0a90cf283c0b1f6c71e8210ce1d7505a218adc
Jeison Barbosa <jdbarbosa95@gmail.com>
2022-06-21 21:03:45 -0500

    rest api

---
+++ src/test/java/com/adaschool/myrestapi/MyrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaschool.myrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
